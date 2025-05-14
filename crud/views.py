25a5d1a23f379e55f6eeacfa421ceb59763e2511
JayRyanG <jamesryangolocino@gmail.com>
2025-05-14 08:57:15 +0800

    Web Application

--- crud/views.py
+++ crud/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Genders
 
 # Create your views here.
 def add_gender(request):
-    return render(request,'gender/AddGender.html')
+    try:
+        if request.method == 'post':
+            gender= request.POSTg.get('gender')
+            Genders.objects.create(gender=gender).save()
+            return HttpResponse('gender added successfully!')
+        else:
+            return render(request, 'gender/AddGender.html')
+    except Exception as e:
+        return HttpResponse(f'Error occurred during add gender:{e}')
