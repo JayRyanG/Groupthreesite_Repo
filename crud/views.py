f0d71d5903c7292fd548a49cd0a86cae107c537f
JayRyanG <jamesryangolocino@gmail.com>
2025-05-14 13:51:27 +0800

    web application

--- crud/views.py
+++ crud/views.py
@@ -1,14 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib import messages
 from .models import Genders
 
 # Create your views here.
+def gender_list(request):
+    try:
+        genders = genders.objects.all() # SELECT * FROM tbl_genders;
+
+        data = {
+            'genders':genders
+        }
+
+        return render(request, 'gender/GendersList.html')
+    except Exception as e:
+        return HttpResponse(f'Error occurred during load genders:{e}')
+    
 def add_gender(request):
     try:
         if request.method == 'POST':
             gender= request.POST.get('gender')
-            Genders.objects.create(gender=gender).save()
-            return HttpResponse('gender added successfully!')
+
+            Genders.objects.create(gender=gender).save() # INSERT INTO tbl_genders (genders) VALUES;
+            messages.success(request,'Gender added successfully!')
+            return render(request, 'gender/AddGender.html')
         else:
             return render(request, 'gender/AddGender.html')
     except Exception as e:
